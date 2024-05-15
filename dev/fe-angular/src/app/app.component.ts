e66248a60bb589c362362ba14ce673f327ede29c
lai2-24 <lai2-24@itismeucci.com>
2024-05-15 10:47:32 +0200

    aggiunto font, icona profilo

--- dev/fe-angular/src/app/app.component.ts
+++ dev/fe-angular/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { CommonModule, JsonPipe, NgIf } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { TutorialsService } from './services/tutorials.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, HttpClientModule],
+  imports: [RouterOutlet, JsonPipe, HttpClientModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
