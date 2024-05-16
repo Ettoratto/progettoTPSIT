78d54de7e23743a4029a3f67bec2e74a2e34287f
Ettoratto <ettore.garofalo.19@gmail.com>
2024-05-17 00:23:28 +0200

    altezze fixate, aggiunta counter

--- dev/fe-angular/src/app/app.component.ts
+++ dev/fe-angular/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { CommonModule, JsonPipe, NgIf } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ActiveUsersComponent } from './components/active-users/active-users.component';
 
 import { TutorialsService } from './services/tutorials.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JsonPipe, HttpClientModule, SidebarComponent],
+  imports: [RouterOutlet, JsonPipe, HttpClientModule, SidebarComponent, ActiveUsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ dev/fe-angular/src/app/components/active-users/active-users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './active-users.component.html',
+  styleUrl: './active-users.component.scss'
+})
+export class ActiveUsersComponent {
+
+}
