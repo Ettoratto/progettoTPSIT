e2723dadceb9e7dbd5cff5268647723efd353272
Ettoratto <ettore.garofalo.19@gmail.com>
2024-05-22 19:36:32 +0200

    non so che ho cambiato ma funziona tutto a me

--- dev/be-springboot/src/main/java/it/ms/api/BeSpringbootApplication.java
+++ dev/be-springboot/src/main/java/it/ms/api/BeSpringbootApplication.java
@@ -3,7 +3,7 @@ package it.ms.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	
 public class BeSpringbootApplication {
 
 	public static void main(String[] args) {
